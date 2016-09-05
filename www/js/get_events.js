f8c13a0dfe28b0a5194e40e60ffdc6fc81475ab8
Gaston Ramirez <gastonframirez@gmail.com>
2016-09-05 18:33:56 -0300

    fixes #14

--- www/js/get_events.js
+++ www/js/get_events.js
@@ -1,12 +1,18 @@
 var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
-		var gobAbiertoAPI_actividades = "/actividad-publica/"
-		var gobAbiertoAPI_evento = "?evento_id=1"
+		var gobAbiertoAPI_actividades = "/actividad-publica/";
+		var gobAbiertoAPI_evento = "?evento_id=1";
+		var page_eventos = "&page=";
+		var pageNumber = getParameterByName('page'); 
+		if (pageNumber == null){
+			pageNumber = 1;
+		}
+		var page_size_str = "&page_size=";
 		var formatJson = "&format=json";
 		var bottom = $('.img-holder').position().top + $('.img-holder').outerHeight(true)+5;
 		$('body').css('padding-top', bottom);
 		$.ajax({
 			dataType: "json",
-			url: gobAbiertoAPI+gobAbiertoAPI_actividades+gobAbiertoAPI_evento+formatJson,
+			url: gobAbiertoAPI+gobAbiertoAPI_actividades+gobAbiertoAPI_evento+page_eventos+pageNumber+page_size_str+pageSize+formatJson,
 			success: handleData
 		});
 		function handleData(data) {
@@ -24,6 +30,21 @@ var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
 		 			}
 	 			}
 			});
+			var htmlPrvNxt = '<div class="row evento"><nav aria-label="..."><ul class="pager">';
+			if (data.previous != null){
+				var prevPage = getParameterByName('page', data.previous);
+				if (prevPage == null){
+					prevPage = 1;
+				}
+				htmlPrvNxt += '<li class="previous"><a href="index.html?page='+prevPage+'" class="pull-left pager-li page-prev"><span aria-hidden="true">&larr;</span>Anterior</a></li>';
+			}
+			if (data.next != null){
+				var nextPage = getParameterByName('page', data.next);
+				htmlPrvNxt += '<li class="next"><a href="index.html?page='+nextPage+'" class="pull-right pager-li page-next">Siguiente<span aria-hidden="true">&rarr;</span></a></li>';
+			}
+			htmlPrvNxt += '</div>'
+			$('#event-list').append(htmlPrvNxt);
+
 			$('#loading').hide();
 
 		}
---
+++ www/js/page_size.js
@@ -0,0 +1,1 @@
+var pageSize = 25;
