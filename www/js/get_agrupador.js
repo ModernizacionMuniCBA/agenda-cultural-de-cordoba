f5b8b32315ae3de814001d0f08e11d002267eda2
Gaston Ramirez <gastonframirez@gmail.com>
2016-09-25 21:30:28 -0300

    fixes #25

--- www/js/get_agrupador.js
+++ www/js/get_agrupador.js
@@ -33,15 +33,13 @@ var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
 			$.each(data.results, function(i, item) {
 				if(item.inicia != null){
 		 			var event_date_aux = new Date(item.inicia);
-		 			var now = new Date();
-		 			if(event_date_aux > now ){
-			 			if (item.image != undefined ){
-				 			var event_image = item.image.original.replace(/^http:\/\//i, 'https://');	
-			 			}else{
-				 			var event_image = "img/default-event.png";
-			 			}
-			 			$('#event-list').append('<div class="row evento"><a href="evento.html#act-'+item.id+'" class="evento"><div class="col-xs-12"><div class="row"><div class="col-xs-3 col-sm-1"><div class="circle-image" style="background-image: url('+event_image+')"></div></div><div class="col-xs-9"><div class="row"><div class="col-xs-12"><h5 class="event-title">'+item.titulo+'</h5></div></div><div class="row"><div class="col-xs-12"><p class="event-date">'+dateFormat(item.inicia, "dddd dd 'de' mmmm, h:MM TT")+'</p></div></div></div></div></div></a></div>');
-		 			}
+		 			if (item.image != undefined ){
+				 		var event_image = item.image.original.replace(/^http:\/\//i, 'https://');	
+			 		}else{
+				 		var event_image = "img/default-event.png";
+			 		}
+			 		$('#event-list').append('<div class="row evento"><a href="evento.html#act-'+item.id+'" class="evento"><div class="col-xs-12"><div class="row"><div class="col-xs-3 col-sm-1"><div class="circle-image" style="background-image: url('+event_image+')"></div></div><div class="col-xs-9"><div class="row"><div class="col-xs-12"><h5 class="event-title">'+item.titulo+'</h5></div></div><div class="row"><div class="col-xs-12"><p class="event-date">'+dateFormat(item.inicia, "dddd dd 'de' mmmm, h:MM TT")+'</p></div></div></div></div></div></a></div>');
+
 	 			}
 			});
 			var htmlPrvNxt = '<div class="row evento"><nav aria-label="..."><ul class="pager">';
