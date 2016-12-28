0988db52ce44316572551cf75288b8d0cca4e0cd
Gaston Ramirez <gastonframirez@gmail.com>
2016-12-28 11:34:31 -0300

    agrega fecha de finalizacion

--- www/js/get_activities.js
+++ www/js/get_activities.js
@@ -19,17 +19,20 @@ var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
 		var start_date = new Date();
 		$('#event-list').html('');
 		function handleData(data) {
-			$.each(data.results, function(i, item) {
-				if(item.inicia != null){
-					// 	var event_date_aux = new Date(item.inicia);
-					start_date = new Date(dateFormat(item.inicia, "mmmm dd, yyyy h:MM TT"));
-			 		if (item.image != undefined ){
-				 		var event_image = item.image.thumbnail.replace(/^http:\/\//i, 'https://');
-			 		}else{
-						var event_image = "img/default-event.png";
-					}
-					var yourString = item.descripcion; //replace with your string.
-					var maxLength = 120; // maximum number of characters to extract
+			if (data.count == 0) {
+				$('#event-list').append('<div>En este momento no hay actividades disponibles.</div>');
+			}else{
+				$.each(data.results, function(i, item) {
+					if(item.inicia != null){
+						// 	var event_date_aux = new Date(item.inicia);
+						start_date = new Date(dateFormat(item.inicia, "mmmm dd, yyyy h:MM TT"));
+				 		if (item.image != undefined ){
+					 		var event_image = item.image.thumbnail.replace(/^http:\/\//i, 'https://');
+				 		}else{
+							var event_image = "img/default-event.png";
+						}
+						var yourString = item.descripcion; //replace with your string.
+						var maxLength = 120; // maximum number of characters to extract
 
 					//trim the string to the maximum length
 					if(yourString.length > 120 ){
@@ -40,13 +43,15 @@ var gobAbiertoAPI = "https://gobiernoabierto.cordoba.gob.ar/api";
 					}
 					var month = dateFormat(item.inicia, "mmm");
 					var day = dateFormat(item.inicia, "dd");
+					var month_end = dateFormat(item.termina, "mmm");
+					var day_end = dateFormat(item.termina, "dd");
 					//re-trim if we are in the middle of a word
 					// 	$('#event-list').append('<div class="row evento"><a href="actividad.html#act-'+item.id+'" class="evento"><div class="col-xs-12"><div class="row"><div class="col-xs-3 col-sm-1"><div class="circle-image" style="background-image: url('+event_image+')"></div></div><div class="col-xs-9"><div class="row"><div class="col-xs-12"><h5 class="event-title">'+item.titulo+'</h5></div></div><div class="row"><div class="col-xs-12"><p class="event-date">'+dateFormat(item.inicia, "dddd dd 'de' mmmm, h:MM TT")+'</p></div></div></div></div></div></a></div>');
 					// $('#event-list').append('<div class="row"><a href="actividad.html#act-'+item.id+'"><div class="col-sm-12 act-card-holder"><div class="act-card"><div class="row"><div class="col-xs-9"><h1 class="card-title">'+item.titulo+'</h1><div class="event-divider"></div></div><div class="col-xs-3 event-date"><p>'+month+'<br/>'+day+'</p></div></div><div class="row"><div class="col-xs-3 act-card-img"><div class="evento-img-cont" style="background-image: url('+event_image+');"></div></div><div class="col-xs-9 act-card-info">'+trimmedString+'</div></div><div class="act-card-icons"><a href="actividad.html#act-'+item.id+'" class="pull-right btn vermas">Ver Más</a></div></div></div></a></div>');
-					$('#event-list').append('<div class="col-xs-12 col-md-4 act-card-holder"><a href="actividad.html#act-'+item.id+'"><div class="act-card"><div class="row"><div class="col-xs-9"><h1 class="card-title">'+item.titulo+'</h1><div class="event-divider"></div></div><div class="col-xs-3 event-date"><p>'+month+'<br/>'+day+'</p></div></div><div class="row"><div class="col-xs-3 act-card-img padding-15"><div class="evento-img-cont" style="background-image: url('+event_image+');"></div></div><div class="col-xs-9 act-card-info">'+trimmedString+'</div></div><div class="act-card-icons"><a href="actividad.html#act-'+item.id+'" class="pull-right icon-act-card"><i class="material-icons">add</i></a></div></div></a></div>');
-
-	 			}
-			});
+					$('#event-list').append('<div class="col-xs-12 col-md-4 act-card-holder"><a href="actividad.html#act-'+item.id+'"><div class="act-card"><div class="row"><div class="col-xs-9"><h1 class="card-title">'+item.titulo+'</h1><div class="event-divider"></div></div><div class="col-xs-3 event-date"><p>'+month+'<br/>'+day+'<br/>-<br/>'+month_end+'<br/>'+day_end+'</p></div></div><div class="row"><div class="col-xs-3 act-card-img padding-15"><div class="evento-img-cont" style="background-image: url('+event_image+');"></div></div><div class="col-xs-9 act-card-info">'+trimmedString+'</div></div><div class="act-card-icons"><a href="actividad.html#act-'+item.id+'" class="pull-right icon-act-card"><i class="material-icons">add</i></a></div></div></a></div>');
+		 			}
+				});
+			}
 			var htmlPrvNxt = '<div class="row evento"><nav aria-label="..."><ul class="pager">';
 			if (data.previous != null){
 				var prevPage = getParameterByName('page', data.previous);
